1123b846ac8897e598707438a0b03bb6f9893ec0
jwork <jkopatsis@gmail.com>
2024-02-13 16:07:57 -0800

    fade in intersection observer

--- js/dom.js
+++ js/dom.js
@@ -1,18 +1,18 @@
-var modeswitch = () =>{
-   
-let switchlogo = document.querySelector('.modeswitch')
-   
-if (switchlogo != null){
-       switchlogo.addEventListener('click', ()=>{
-           if (switchlogo.classList.contains('lightmode')){
-               switchlogo.src = './assets/ic4b.png'
-               switchlogo.classList.add('darkmode')
-               switchlogo.classList.remove('lightmode')
-               document.querySelectorAll('.inner').forEach(element => {
-                   element.style.backgroundColor = '#262C2E'
+var modeswitch = () => {
+
+    let switchlogo = document.querySelector('.modeswitch')
+
+    if (switchlogo != null) {
+        switchlogo.addEventListener('click', () => {
+            if (switchlogo.classList.contains('lightmode')) {
+                switchlogo.src = './assets/ic4b.png'
+                switchlogo.classList.add('darkmode')
+                switchlogo.classList.remove('lightmode')
+                document.querySelectorAll('.inner').forEach(element => {
+                    element.style.backgroundColor = '#262C2E'
                 })
-            } else{
-               switchlogo.src = './assets/ic4a.png'
+            } else {
+                switchlogo.src = './assets/ic4a.png'
                 switchlogo.classList.remove('darkmode')
                 switchlogo.classList.add('lightmode')
                 document.querySelectorAll('.inner').forEach(element => {
@@ -21,30 +21,57 @@ if (switchlogo != null){
             }
         })
     }
-  }
+}
 
-  var links = [document.querySelector('#link1'), document.querySelector('#link2'), document.querySelector('#link3'), document.querySelector('#link4')]
+var links = [document.querySelector('#link1'), document.querySelector('#link2'), document.querySelector('#link3'), document.querySelector('#link4')]
 
-  let observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry, i) => {
-        if (entry.isIntersecting) {
-            links[i].style.color = '#b10404'
-        } else{
-            links[i].style.color = ''
-        }
-    });
-}, { threshold: [0.1] });
+// let observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry, i) => {
+//         if (entry.isIntersecting) {
+//             links[i].style.color = '#b10404'
+//         } else {
+//             links[i].style.color = ''
+//         }
+//     });
+// }, { threshold: [0.1] });
 
 
-var idColor = () =>{
+var idColor = () => {
 
     var screens = [document.querySelector('#one'), document.querySelector('#two'), document.querySelector('#three'), document.querySelector('#four')]
 
-    observer.observe(screens)
+    // observer.observe(screens)
 }
 
- 
-document.addEventListener("DOMContentLoaded", function() {
+
+var fadeIn = () => {
+    const elementsToLoadIn = new Set([...document.querySelectorAll(".whole"), ...document.querySelectorAll("#title")])
+    elementsToLoadIn.forEach((el) => {
+        el.classList.add('loadin');
+    });
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3,
+    };
+
+    function observerCallback(entries) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('loaded');
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    elementsToLoadIn.forEach((el) => observer.observe(el));
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
     modeswitch()
     idColor()
+    fadeIn()
 })
